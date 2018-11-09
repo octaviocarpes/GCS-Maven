d41e85117ed0320dbd45fcbcda6ef1be1d097d59
octavio.carpes <octavio.carpes10@gmail.com>
2018-11-08 22:38:24 -0200

    Hotfix start.sh

--- start.sh
+++ start.sh
@@ -3,6 +3,6 @@ echo "O comando mvn clean limpa os arquivos e diretorios criados pelo seu build.
 mvn clean &&
 echo "O comando install roda todos os testes, caso passem ele junta todas as dependencias do código e gera o executavel caso o contrário o build é cancelado." &&
 mvn install &&
-echo "Apps o build basta executar o app gerado."
+echo "Apos build basta executar o app gerado."
 cd target && java -jar demo-0.0.1-SNAPSHOT.jar
 
